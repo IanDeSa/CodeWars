ee32eac63f5ced4fe75e12df717142edc29509f4
Ian Santos <contato.iansantos@gmail.com>
2022-09-23 00:33:47 -0400

    Feat: adiciona duas funções: binaryArrayToNumber converte binário para decimal e numberDecimalToBinary converte decimal para binário

---
+++ codewars.js
@@ -0,0 +1,12 @@
+const binaryArrayToNumber = arr => {
+  return parseInt(arr, 2);
+};
+
+const numberDecimalToBinary = arr => {
+  return parseInt(arr, 10).toString(2);
+};
+
+
+
+console.log(binaryArrayToNumber('1111'));
+console.log(numberDecimalToBinary('15'));
